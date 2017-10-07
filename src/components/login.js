1949d8876e19c9eaedf7e35c5c072bb10d27d783
Elizabeth Blackburn <elizabeth.blackburn@wendys.com>
2017-10-07 15:44:57 -0400

    misc setup/housekeeping, incl updated README

--- src/components/login.js
+++ src/components/login.js
@@ -19,3 +19,6 @@ class Login extends Component {
 }
 
 export default Login;
+
+//todo: actually read this, dummy
+// https://medium.com/@robince885/how-to-do-twitter-authentication-with-react-and-restful-api-e525f30c62bb
