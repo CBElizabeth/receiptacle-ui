4568caab8fda4b459efcbb4808a364ee355e25ae
Elizabeth Blackburn <elizabeth.blackburn@wendys.com>
2017-10-07 15:40:32 -0400

    add responsive breakpoints and collapsing navigation for tablet + phone

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -4,18 +4,15 @@ import '../styles/navigationBar.css';
 
 function NavigationBar() {
     const accountText = 'login';
-    const activeStyle = {
-        color: '#69FFF1',
-        fontSize: '1.4em',
-    };
+
     return (
         <section id="navBar">
-            <h1>receiptacle</h1>
-            <NavLink exact className="button" to="/" activeStyle={activeStyle}>home</NavLink>
-            <NavLink className="button" to="/block" activeStyle={activeStyle}>block</NavLink>
-            <NavLink className="button" to="/receipts" activeStyle={activeStyle}>receipts</NavLink>
-            <NavLink className="button" to="/settings" activeStyle={activeStyle}>settings</NavLink>
-            <NavLink className="button" to="/login" activeStyle={activeStyle}>{accountText}</NavLink>
+            <h1 className="logo">receiptacle</h1>
+            <NavLink exact className="button" to="/" activeClassName="active">home</NavLink>
+            <NavLink className="button" to="/block" activeClassName="active">block</NavLink>
+            <NavLink className="button" to="/receipts" activeClassName="active">receipts</NavLink>
+            <NavLink className="button" to="/settings" activeClassName="active">settings</NavLink>
+            <NavLink className="button" to="/login" activeClassName="active">{accountText}</NavLink>
         </section>
     );
 }
@@ -25,7 +22,5 @@ function NavigationBar() {
 // };
 
 //todo: disable settings if !loggedIn
-//todo: check path to determine which button has class current
-//todo: collapse options for tablet/phone
 
 export default NavigationBar;
